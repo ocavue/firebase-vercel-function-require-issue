882206bb10f0016280142d8fe9988f189b26ad97
ocavue <ocavue@gmail.com>
2021-11-28 17:50:44 +0800

    add firebase-packages.ts

--- pages/api/hellofirebase.ts
+++ pages/api/hellofirebase.ts
@@ -1,9 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import * as firebaseApp from "firebase/app";
-import * as firebaseFirestore from "firebase/firestore";
-import * as firebaseAdmin from "firebase-admin";
-import { addDoc } from "@firebase/firestore";
+import { auth } from "../../services/firebase-debug";
 
 type Data = Record<string, string | number>;
 
@@ -18,10 +15,8 @@ export default function handler(
   res.status(200).json({
     name: "Hello firebase",
     time: new Date().toISOString(),
-    firebaseApp: trim(String(firebaseApp.getApp)),
-    firebaseFirestore: trim(String(firebaseFirestore.addDoc)),
-    firebaseAdmin: trim(String(firebaseAdmin.SDK_VERSION)),
-    addDoc: trim(String(addDoc)),
     version: 1,
+
+    auth: trim(String(auth)),
   });
 }

03df541318c315f2e4f90d549ee98eb79ae694e4
ocavue <ocavue@gmail.com>
2021-11-28 16:00:02 +0800

    use firebase

--- pages/api/hellofirebase.ts
+++ pages/api/hellofirebase.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { addDoc } from "firebase/firestore";
+import * as firebaseApp from "firebase/app";
+import * as firebaseFirestore from "firebase/firestore";
+import * as firebaseAdmin from "firebase-admin";
+import { addDoc } from "@firebase/firestore";
 
-type Data = {
-  name: string;
-  time: string;
-  addDoc: string;
-  version: number;
-};
+type Data = Record<string, string | number>;
+
+function trim(s: string): string {
+  return s.slice(0, 20);
+}
 
 export default function handler(
   req: NextApiRequest,
@@ -16,7 +18,10 @@ export default function handler(
   res.status(200).json({
     name: "Hello firebase",
     time: new Date().toISOString(),
-    addDoc: String(addDoc),
+    firebaseApp: trim(String(firebaseApp.getApp)),
+    firebaseFirestore: trim(String(firebaseFirestore.addDoc)),
+    firebaseAdmin: trim(String(firebaseAdmin.SDK_VERSION)),
+    addDoc: trim(String(addDoc)),
     version: 1,
   });
 }

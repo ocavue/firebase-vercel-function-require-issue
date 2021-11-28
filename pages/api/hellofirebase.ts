ebec25b1263502ac0a8dc56259475e92a47c9fa8
ocavue <ocavue@gmail.com>
2021-11-28 18:07:24 +0800

    try abs import

--- pages/api/hellofirebase.ts
+++ pages/api/hellofirebase.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { auth } from "../../services/firebase-debug";
+import { auth } from "services/firebase-debug";
 
 type Data = Record<string, string | number>;
 

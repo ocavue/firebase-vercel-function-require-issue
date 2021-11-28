57c825e50e4aa3dd9bcdf9c8c41842855e96d675
ocavue <ocavue@gmail.com>
2021-11-28 19:33:30 +0800

    add time

--- pages/oauth.tsx
+++ pages/oauth.tsx
@@ -1,25 +1,28 @@
-import {GetServerSideProps} from 'next'
-import React from 'react'
-import {auth} from 'services/firebase-debug'
+import { GetServerSideProps } from "next";
+import React from "react";
+import { auth } from "services/firebase-debug";
 
 const useFirebaseUser = () => {
-  console.log('Auth:', String(auth))
+  console.log("Auth:", String(auth));
 
-  return {auth: String(auth)}
-}
+  return { auth: String(auth) };
+};
 
-export const getServerSideProps: GetServerSideProps = async ({query, res}) => {
+export const getServerSideProps: GetServerSideProps = async ({
+  query,
+  res,
+}) => {
   return {
     props: {
-      clientId: '_clientId',
-      redirectUri: '_redirectUri',
-      state: '_state',
-      clientName: '_clientName',
-      clientDomain: '_clientDomain',
-      clientInternal: '_clientInternal',
+      clientId: "_clientId",
+      redirectUri: "_redirectUri",
+      state: "_state",
+      clientName: "_clientName",
+      clientDomain: "_clientDomain",
+      clientInternal: "_clientInternal",
     },
-  }
-}
+  };
+};
 
 export default function OAuth({
   redirectUri,
@@ -29,14 +32,19 @@ export default function OAuth({
   clientDomain,
   clientInternal = false,
 }: {
-  redirectUri: string
-  state: string
-  clientId: string
-  clientName: string
-  clientDomain: string
-  clientInternal: boolean
+  redirectUri: string;
+  state: string;
+  clientId: string;
+  clientName: string;
+  clientDomain: string;
+  clientInternal: boolean;
 }) {
-  const {auth} = useFirebaseUser()
+  const { auth } = useFirebaseUser();
 
-  return <div>Hello auth:{String(auth)}</div>
+  return (
+    <div>
+      <p>time: {String(new Date())}</p>
+      <p>auth: {String(auth)}</p>
+    </div>
+  );
 }

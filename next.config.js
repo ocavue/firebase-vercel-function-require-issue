f694401dfc4ce6a92db4addf8cc05f9768d57506
ocavue <ocavue@gmail.com>
2021-11-28 19:26:08 +0800

    add esmExternals

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+"use strict";
+
+const config = {
+  experimental: { esmExternals: "loose" },
+};
+
+module.exports = config;
